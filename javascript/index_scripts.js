4be2d80ef6bda93fe5e65e0e9b816a988adf57c1
Patrick Petro <patrickjpetro@icloud.com>
2022-03-21 18:10:50 -0400

    style updates, add drag and drop interface

--- javascript/index_scripts.js
+++ javascript/index_scripts.js
@@ -1,371 +1,147 @@
 var TimeWorkDay = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"]
 var TimeMilitary = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
-var Table = $("#TimeTable")
+const toDoCol = $("#toDo");
+const inProgressCol = $("#inProgress");
+const doneCol = $("#done");
 
 $(document).ready(function() {
+    var tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    tasks.forEach(function(task) {
+        
+        const listGroup = $("<div id='" + task.id + "'" + "class='list-group m-2 text-center' draggable='true' ondragstart='drag(event)'>");
 
-    var DateDisplay = document.querySelector("#currentDate")
+        const listGroupItem = $("<div id='item_" + task.id + "'" + "class='list-group-item list-group-item-action'>");
+
+        const inputTitle = $("<input id='title_" + task.id + "'" + "placeholder='Task Title' />");
+        inputTitle.val(task.title)
+
+        const textarea = $("<textarea id='details_" + task.id + "'" + "placeholder='Task Details...'>");
+        textarea.text(task.details)
+
+        const smallText = $("<small id='dateCreated_" + task.id + "'" + "class='added-date'>");
+        smallText.text(task.dateCreated);
+    
+        const saveButton = $("<button class='save-button' onclick='save(" + task.id + ")" + "'>");
+        const deleteButton = $("<button class='delete-button' onclick='deleteTask(" + task.id + ")" + "'>");
+    
+        const faSave = $("<i class='fa fa-save'></i>");
+        const faDelete = $("<i class='fa fa-trash'></i>");
+    
+        saveButton.append(faSave);
+        deleteButton.append(faDelete);
+        listGroupItem.append(inputTitle, textarea, smallText, saveButton, deleteButton);
+        listGroup.append(listGroupItem);
+
+        if (task.status === "toDo") {
+            toDoCol.append(listGroup);
+        } else if (task.status === "inProgress"){
+            inProgressCol.append(listGroup);
+        } else {
+            doneCol.append(listGroup); 
+        } 
+    });
+
+    const DateDisplay = $("#currentDate");
+    const addedDate = $("#addedDate");
     var currentDate = moment().format('LL');
     
-    DateDisplay.textContent = currentDate
-
-    var newSaveAllBtn = $("<button>")
-    var newCompleteAllBtn = $("<button>")
-    var FAusb = $("<i>")
-    var FaDelete = $("<i>")
-
-    newSaveAllBtn.text(" (All)")
-    newSaveAllBtn.attr({
-        id: "SaveAllBtn",
-        class: "SaveAllBtn btn btn-primary"
-    })
-
-    FAusb.attr("class", "fa fa-save")
-
-    newSaveAllBtn.prepend(FAusb)
-
-
-    newCompleteAllBtn.text(" (All)")
-    newCompleteAllBtn.attr({
-        id: "CompleteAllBtn",
-        class: "CompleteAllBtn btn btn-danger"
-    })
-    
-    FaDelete.attr("class", "fa fa-trash")
-    newCompleteAllBtn.prepend(FaDelete)
-
-
-    $("#headerBtn").append(newSaveAllBtn, newCompleteAllBtn)
-
-    $("#txt9AM").text(localStorage.getItem("ToDo9AM"))
-    $("#txt10AM").text(localStorage.getItem("ToDo10AM"))
-    $("#txt11AM").text(localStorage.getItem("ToDo11AM"))
-    $("#txt12PM").text(localStorage.getItem("ToDo12PM"))
-    $("#txt1PM").text(localStorage.getItem("ToDo1PM"))
-    $("#txt2PM").text(localStorage.getItem("ToDo2PM"))
-    $("#txt3PM").text(localStorage.getItem("ToDo3PM"))
-    $("#txt4PM").text(localStorage.getItem("ToDo4PM"))
-    $("#txt5PM").text(localStorage.getItem("ToDo5PM"))
+    DateDisplay.text(currentDate)
+    addedDate.text(currentDate)
 });
 
+$(".add-button").click( () => {
+    const letters = "abcdefghijklmnopqrstuvwxyz".split('');
+    // const numbers = "0123456789".split('')
+    const idLength = 6;
+    const idNewTaskArray = [];
 
-function ListTimes() {
-    
-    for (i = 0; i < TimeWorkDay.length; i++) {
-        var newRow = $("<tr>")
+    for (let i = 0; i < idLength; i++) {
+        var letter = letters[Math.floor(Math.random() * letters.length)];
+        // var number = numbers[Math.floor(Math.random() * numbers.length)];
+        idNewTaskArray.push(letter);
+        // idNewTask.push(number);
         
-        var newHour = $("<td>").text(TimeWorkDay[i]);
-        var newToDO = $("<textarea>");
-        var newSaveBtn = $("<button>");
-        var newCompleteBtn = $("<button>");
-        var FAusb = $("<i>")
-        var FaDelete = $("<i>")
-
-        newSaveBtn.attr({
-            class: "SaveBtn btn btn-primary",
-            id: "SaveBtn",
-        });
-
-        FAusb.attr("class", "fa fa-save")
-
-        newSaveBtn.append(FAusb)
-
-        newCompleteBtn.attr({
-            id: "CompleteBtn" + TimeWorkDay[i],
-            class: "CompleteBtn btn btn-danger"
-        });
-
-        FaDelete.attr("class", "fa fa-trash")
-        newCompleteBtn.append(FaDelete)
-
-        newSaveBtn.attr("id", "Btn" + TimeWorkDay[i])
-        newHour.attr({
-            id: "label" + TimeWorkDay[i],
-            class: "hourlbl"
-        });
-        
-        newToDO.attr("id", "txt" + TimeWorkDay[i])
-
-
-        newRow.append(newHour, newToDO, newCompleteBtn, newSaveBtn)
-    
-        $("tbody").append(newRow);
-
-        var currentHour = moment().hour()
-        var NineAM = $("#txt9AM")
-            if (currentHour < 9){
-                NineAM.attr("class", "futureHr")
-            } else if (currentHour === 9) {
-                NineAM.attr("class", "currentHr")
-            } else {
-                NineAM.attr("class", "pastHr")
-            }
-
-        var TenAM = $("#txt10AM")
-            if (currentHour < 10){
-                TenAM.attr("class", "futureHr")
-            } else if (currentHour === 10) {
-                TenAM.attr("class", "currentHr")
-            } else {
-                TenAM.attr("class", "pastHr")
-            }
-
-        var ElevenAM = $("#txt11AM")
-            if (currentHour < 11){
-                ElevenAM.attr("class", "futureHr")
-            } else if (currentHour === 11) {
-                ElevenAM.attr("class", "currentHr")
-            } else {
-                ElevenAM.attr("class", "pastHr")
-            }
-
-        var TwelveAM = $("#txt12PM")
-            if (currentHour < 12){
-                TwelveAM.attr("class", "futureHr")
-            } else if (currentHour === 12) {
-                TwelveAM.attr("class", "currentHr")
-            } else {
-                TwelveAM.attr("class", "pastHr")
-            }
-            
-        var OnePM = $("#txt1PM")
-            if (currentHour < 13){
-                OnePM.attr("class", "futureHr")
-            } else if (currentHour === 13) {
-                OnePM.attr("class", "currentHr")
-            } else {
-                OnePM.attr("class", "pastHr")
-            }
-
-        var TwoPM = $("#txt2PM")
-            if (currentHour < 14){
-                TwoPM.attr("class", "futureHr")
-            } else if (currentHour === 14) {
-                TwoPM.attr("class", "currentHr")
-            } else {
-                TwoPM.attr("class", "pastHr")
-            }
-
-        var ThreePM = $("#txt3PM")
-            if (currentHour < 15){
-                ThreePM.attr("class", "futureHr")
-            } else if (currentHour === 15) {
-                ThreePM.attr("class", "currentHr")
-            } else {
-                ThreePM.attr("class", "pastHr")
-            }
-
-            var FourPM = $("#txt4PM")
-            if (currentHour < 16){
-                FourPM.attr("class", "futureHr")
-            } else if (currentHour === 16) {
-                FourPM.attr("class", "currentHr")
-            } else {
-                FourPM.attr("class", "pastHr")
-            }
-
-            var FivePM = $("#txt5PM")
-            if (currentHour < 17){
-                FivePM.attr("class", "futureHr")
-            } else if (currentHour === 17) {
-                FivePM.attr("class", "currentHr")
-            } else {
-                FivePM.attr("class", "pastHr")
-            }
     }
+    var idNewTask = idNewTaskArray.join('').toString();
+    console.log(idNewTask)
+    var currentDate = moment().format('l, h:mm:ss a');
+    
+    const listGroup = $("<div id='" + idNewTask + "'" + "class='list-group m-2 text-center' draggable='true' ondragstart='drag(event)'>");
+    const listGroupItem = $("<div id='item_" + idNewTask + "'" + "class='list-group-item list-group-item-action'>");
+    const inputTitle = $("<input id='title_" + idNewTask + "'" + "placeholder='Task Title' />");
+    const textarea = $("<textarea id='details_" + idNewTask + "'" + "placeholder='Task Details...'>")
+    const smallText = $("<small id='dateCreated_" + idNewTask + "'" + "class='added-date'>");
+    smallText.text("Created: " + currentDate);
+
+    const saveButton = $("<button class='save-button' onclick='save(" + idNewTask + ")" + "'>");
+    const deleteButton = $("<button class='delete-button' onclick='deleteTask(" + idNewTask + ")" + "'>");
+
+    const faSave = $("<i class='fa fa-save'></i>");
+    const faDelete = $("<i class='fa fa-trash'></i>");
+
+    saveButton.append(faSave);
+    deleteButton.append(faDelete);
+
+    listGroupItem.append(inputTitle, textarea, smallText, saveButton, deleteButton);
+    listGroup.append(listGroupItem);
+    toDoCol.append(listGroup);
+
+})
+
+function save(task) {
+    const title = $("#title_" + task.id).val();
+    const details = $("#details_" + task.id).val();
+    const dateCreated = $("#dateCreated_" + task.id).text();
+
+    // load local storage
+    var tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+
+    // create task object
+    var newTask = {
+        "id": task.id, 
+        "title": title,
+        "details": details,
+        "dateCreated": dateCreated,
+        "status": "toDo",
+    };
+
+    // Push to local storage array
+    tasks.push(newTask);
+
+    // save to local storage
+    localStorage.setItem("tasks", JSON.stringify(tasks)); 
 }
-ListTimes();
 
-//Click Events
-$("#Btn9AM").click(function(){
+function deleteTask(task) {
+    // console.log(task.id);
+    var tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+ 
 
-    var Txt9AM = $("#txt9AM").val()
-    localStorage.setItem("ToDo9AM", Txt9AM)
+    for (let i = 0; i < tasks.length; i++) {
+        if (task.id === tasks[i].id) {
+            const index = tasks.findIndex(item => item.id === task.id);
+            tasks.splice(index, 1)
 
-    window.location.reload()
-
-});
-
-$("#Btn10AM").click(function(){
-    
-    var Txt10AM = $("#txt10AM").val()
-    localStorage.setItem("ToDo10AM", Txt10AM)
-
-    window.location.reload()
-
-});
-
-$("#Btn11AM").click(function(){
-   
-    var Txt11AM = $("#txt11AM").val()
-    localStorage.setItem("ToDo11AM", Txt11AM)
-
-    window.location.reload()
-      
-});
-
-$("#Btn12PM").click(function(){
-   
-    var Txt12PM = $("#txt12PM").val()
-    localStorage.setItem("ToDo12PM", Txt12PM)
-
-    window.location.reload()
-     
-});
-
-$("#Btn1PM").click(function(){
-    
-    var Txt1PM = $("#txt1PM").val()
-    localStorage.setItem("ToDo1PM", Txt1PM)
-
-    window.location.reload()
-});
-
-$("#Btn2PM").click(function(){
-    
-    var Txt2PM = $("#txt2PM").val()
-    localStorage.setItem("ToDo2PM", Txt2PM)
-
-    window.location.reload()
-     
-});
-
-$("#Btn3PM").click(function(){
-   
-    var Txt3PM = $("#txt3PM").val()
-    localStorage.setItem("ToDo3PM", Txt3PM)
-
-    window.location.reload()
-     
-});
-
-
-$("#Btn4PM").click(function(){
-   
-    var Txt4PM = $("#txt4PM").val()
-    localStorage.setItem("ToDo4PM", Txt4PM)
-
-    window.location.reload()
-      
-});
-
-$("#Btn5PM").click(function(){
-    
-    var Txt5PM = $("#txt5PM").val()
-    localStorage.setItem("ToDo5PM", Txt5PM)
-
-    window.location.reload()
-     
-});
-
-$("#CompleteBtn9AM").click(function(){
-
-    localStorage.removeItem("ToDo9AM")
-    window.location.reload()
-    
-});
-
-$("#CompleteBtn10AM").click(function(){
-
-    localStorage.removeItem("ToDo10AM")
-    window.location.reload()
-    
-});
-
-$("#CompleteBtn11AM").click(function(){
-
-    localStorage.removeItem("ToDo11AM")
-    window.location.reload()
-    
-});
-
-$("#CompleteBtn12PM").click(function(){
-
-    localStorage.removeItem("ToDo12PM")
-    window.location.reload()
-    
-});
-
-$("#CompleteBtn1PM").click(function(){
-
-    localStorage.removeItem("ToDo1PM")
-    window.location.reload()
-    
-});
-
-$("#CompleteBtn2PM").click(function(){
-
-    localStorage.removeItem("ToDo2PM")
-    window.location.reload()
-    
-});
-
-$("#CompleteBtn3PM").click(function(){
-
-    localStorage.removeItem("ToDo3PM")
-    window.location.reload()
-    
-});
-
-$("#CompleteBtn4PM").click(function(){
-
-    localStorage.removeItem("ToDo4PM")
-    window.location.reload()
-    
-});
-
-$("#CompleteBtn5PM").click(function(){
-
-    localStorage.removeItem("ToDo5PM")
-    window.location.reload()
-    
-});
-
-$(document).on("click", "#SaveAllBtn", function() {
-   
-    var Txt9AM = $("#txt9AM").val()
-    var Txt10AM = $("#txt10AM").val()
-    var Txt11AM = $("#txt11AM").val()
-    var Txt12PM = $("#txt12PM").val()
-    var Txt1PM = $("#txt1PM").val()
-    var Txt2PM = $("#txt2PM").val()
-    var Txt3PM = $("#txt3PM").val()
-    var Txt4PM = $("#txt4PM").val()
-    var Txt5PM = $("#txt5PM").val()
-        
-    localStorage.setItem("ToDo9AM", Txt9AM)
-    localStorage.setItem("ToDo10AM", Txt10AM)
-    localStorage.setItem("ToDo11AM", Txt11AM)
-    localStorage.setItem("ToDo12PM", Txt12PM)
-    localStorage.setItem("ToDo1PM", Txt1PM)
-    localStorage.setItem("ToDo2PM", Txt2PM)
-    localStorage.setItem("ToDo3PM", Txt3PM)
-    localStorage.setItem("ToDo4PM", Txt4PM)
-    localStorage.setItem("ToDo5PM", Txt5PM)
-
-    window.location.reload()
-});
-
-$(document).on("click", "#CompleteAllBtn", function() {
-
-    var Confirm = confirm("Are you sure you want to complete all tasks?")
-        if (Confirm === true){
-
-            localStorage.removeItem("ToDo9AM")
-            localStorage.removeItem("ToDo10AM")
-            localStorage.removeItem("ToDo11AM")
-            localStorage.removeItem("ToDo12PM")
-            localStorage.removeItem("ToDo1PM")
-            localStorage.removeItem("ToDo2PM")
-            localStorage.removeItem("ToDo3PM")
-            localStorage.removeItem("ToDo4PM")
-            localStorage.removeItem("ToDo5PM")
-
-            window.location.reload()
-        } 
-        else {
-            
+             // save to local storage
+            localStorage.setItem("tasks", JSON.stringify(tasks)); 
+            window.location.reload();
         }
-});
+    }
+    // let index = selectedTask.findIndex(id => id == selectedTask.id)
+    // console.log(selectedTask);
+}
 
-
+// Drag & Drop Functionality
+function allowDrop(event) {
+    event.preventDefault();
+}
+  
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+  
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+}
