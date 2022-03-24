1344738a267579b45dfed32d536b6e23c9f27890
Patrick Petro <patrickjpetro@icloud.com>
2022-03-24 14:45:57 -0400

    feat: add onkeyup events to save tasks when info is typed

--- javascript/index_scripts.js
+++ javascript/index_scripts.js
@@ -4,6 +4,9 @@ const toDoCol = $("#toDo");
 const inProgressCol = $("#inProgress");
 const doneCol = $("#done");
 
+// load local storage
+var tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+
 $(document).ready(function() {
     const dissmissedWelcomeAlert = localStorage.getItem("dissmissedWelcomeAlert");
     if (dissmissedWelcomeAlert === "true") {
@@ -14,27 +17,24 @@ $(document).ready(function() {
             localStorage.setItem("dissmissedWelcomeAlert", true)
         } else {
             localStorage.setItem("dissmissedWelcomeAlert", true)
-        }
-    }
+        };
+    };
 
-    var tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
     tasks.forEach(function(task) {
         const listGroup = $("<div id='" + task.id + "'" + "class='list-group m-2 text-center' draggable='true' ondragstart='drag(event)'>");
         const listGroupItem = $("<div id='item_" + task.id + "'" + "class='list-group-item list-group-item-action'>");
-        const inputTitle = $("<input id='title_" + task.id + "'" + "placeholder='Title' />");
+        const inputTitle = $("<input id='title_" + task.id + "'" + "onkeyup='editTask(" + task.id + ")" + "' placeholder='Title' />");
         inputTitle.val(task.title)
-        const textarea = $("<textarea id='details_" + task.id + "'" + "placeholder='Details...'>");
+        const textarea = $("<textarea id='details_" + task.id + "'" + "onkeyup='editTask(" + task.id + ")" + "' placeholder='Details...'>")
         textarea.text(task.details)
         const smallText = $("<p id='dateCreated_" + task.id + "'" + "class='added-date'>");
         smallText.text(task.dateCreated);
-        const saveButton = $("<button class='save-button' onclick='save(" + task.id + ")" + "'>");
         const deleteButton = $("<button class='delete-button' onclick='deleteTask(" + task.id + ")" + "'>");
         const faSave = $("<i class='fa fa-save'></i>");
         const faDelete = $("<i class='fa fa-trash'></i>");
     
-        saveButton.append(faSave);
         deleteButton.append(faDelete);
-        listGroupItem.append(inputTitle, textarea, smallText, saveButton, deleteButton);
+        listGroupItem.append(inputTitle, textarea, smallText, deleteButton);
         listGroup.append(listGroupItem);
 
         if (task.status === "toDo") {
@@ -66,81 +66,85 @@ $(".add-button").click( () => {
     }
     const idNewTask = idNewTaskArray.join('').toString();
     const currentDate = moment().format('l, h:mm:ss a');
-    const listGroup = $("<div id='" + idNewTask + "'" + "class='list-group m-2 text-center' draggable='true' ondragstart='drag(event)'>");
+    const listGroup = $("<div id='" + idNewTask + "'" + "class='list-group m-2 text-center'  draggable='true' ondragstart='drag(event)'>");
     const listGroupItem = $("<div id='item_" + idNewTask + "'" + "class='list-group-item list-group-item-action'>");
-    const inputTitle = $("<input id='title_" + idNewTask + "'" + "placeholder='Title' />");
-    const textarea = $("<textarea id='details_" + idNewTask + "'" + "placeholder='Details...'>")
+    const inputTitle = $("<input id='title_" + idNewTask + "'" + "onkeyup='editTask(" + idNewTask + ")" + "' placeholder='Title' />");
+    const textarea = $("<textarea id='details_" + idNewTask + "'" + "onkeyup='editTask(" + idNewTask + ")" + "' placeholder='Details...'>");
     const smallText = $("<p id='dateCreated_" + idNewTask + "'" + "class='added-date'>");
     smallText.text("Created: " + currentDate);
-    const saveButton = $("<button class='save-button' onclick='save(" + idNewTask + ")" + "'>");
     const deleteButton = $("<button class='delete-button' onclick='deleteTask(" + idNewTask + ")" + "'>");
-    const faSave = $("<i class='fa fa-save'></i>");
     const faDelete = $("<i class='fa fa-trash'></i>");
 
-    saveButton.append(faSave);
     deleteButton.append(faDelete);
-    listGroupItem.append(inputTitle, textarea, smallText, saveButton, deleteButton);
+    listGroupItem.append(inputTitle, textarea, smallText, deleteButton);
     listGroup.append(listGroupItem);
     toDoCol.append(listGroup);
 
-})
+});
 
-function save(task) {
+function editTask(task) {
     const title = $("#title_" + task.id).val();
     const trimmedTitle = $.trim(title);
     const details = $("#details_" + task.id).val().trim();
     const trimmedDetails = $.trim(details);
     const dateCreated = $("#dateCreated_" + task.id).text();
-
-    // load local storage
-    var tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
-
-    // create task object
-    var newTask = {
-        "id": task.id, 
-        "title": trimmedTitle,
-        "details": trimmedDetails,
-        "dateCreated": dateCreated,
-        "status": "toDo",
-    };
-
-    // Push to local storage array
-    tasks.push(newTask);
-
-    // save to local storage
-    localStorage.setItem("tasks", JSON.stringify(tasks)); 
+ 
+    if (tasks.length > 0 && tasks.some(item => item.id === task.id)) {
+        for (let i = 0; i < tasks.length; i++) {
+            if (task.id === tasks[i].id) {
+                tasks[i].title = trimmedTitle;
+                tasks[i].details = trimmedDetails;
     
-    alert("Task Saved!");
-}
+                // save to local storage
+                localStorage.setItem("tasks", JSON.stringify(tasks)); 
+            };
+        };  
+    } else if (tasks.length === 0 || !tasks.some(item => item.id === task.id)) {
+        // create task object
+        var newTask = {
+            "id": task.id, 
+            "title": trimmedTitle,
+            "details": trimmedDetails,
+            "dateCreated": dateCreated,
+            "status": "toDo",
+        };
+        // Push to local storage array
+        tasks.push(newTask);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    };
+};
 
 function deleteTask(task) {
     // console.log(task.id);
-    var tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
-    for (let i = 0; i < tasks.length; i++) {
-        if (task.id === tasks[i].id) {
-            const index = tasks.findIndex(item => item.id === task.id);
-            tasks.splice(index, 1)
-
-             // save to local storage
-            localStorage.setItem("tasks", JSON.stringify(tasks)); 
-            window.location.reload();
-        }
+    if (confirm("Delete Task?")) {
+        for (let i = 0; i < tasks.length; i++) {
+            if (task.id === tasks[i].id) {
+                const index = tasks.findIndex(item => item.id === task.id);
+                tasks.splice(index, 1);
+    
+                 // save to local storage
+                localStorage.setItem("tasks", JSON.stringify(tasks)); 
+                window.location.reload();
+            };
+        };
+    } else {
+        
     }
-}
+    
+};
 
 // Drag & Drop Functionality
 function allowDrop(event) {
     event.preventDefault();
-}
+};
   
 function drag(event) {
     event.dataTransfer.setData("text", event.target.id);
-}
+};
   
 function drop(event) {
     event.preventDefault();
     var data = event.dataTransfer.getData("text");
-    var tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
     for (let i = 0; i < tasks.length; i++) {
         if (data === tasks[i].id) {
             const index = tasks.findIndex(item => item.id === data);
@@ -150,10 +154,10 @@ function drop(event) {
                 tasks[index].status = "inProgress"
             } else {
                 tasks[index].status = "done"
-            }
+            };
              // save to local storage
             localStorage.setItem("tasks", JSON.stringify(tasks)); 
-        }
-    }
+        };
+    };
     event.target.appendChild(document.getElementById(data));
-}
+};
